0bc591c2a54489bffb93dfc94b9d81c8c392e5dd
psiddharth709 <psiddharth709@gmail.com>
2023-12-02 18:28:22 +0000

    Check Types  and Data Types

---
+++ Js_Basic/varibial.js
@@ -0,0 +1,17 @@
+const uniqueid = 236
+let email = "psiddharth@google.com"
+var password = "23sidd"
+passwordcity = "Vadodara"
+let newcity;
+// uniqueid = 2
+
+email = "psiddarth@gmail.com"
+password = "25365"
+passwordcity = "nitg"
+
+// console.log(uniqueid)
+
+// console.table([email,password,passwordcity,newcity])
+
+
+console.log(typeof uniqueid);
